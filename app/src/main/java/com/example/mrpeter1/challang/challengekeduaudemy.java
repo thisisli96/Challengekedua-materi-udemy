8594b03eed83bf3bb1e088ac6d2da97664d9eb92
muh ikhsan <thisisli96@yahoo.com>
2019-11-24 12:42:01 +0700

    coba 3 challenge2 udemy layout dengan gambar FIX

--- app/src/main/java/com/example/mrpeter1/challang/challengekeduaudemy.java
+++ app/src/main/java/com/example/mrpeter1/challang/challengekeduaudemy.java
@@ -7,9 +7,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class challengekeduaudemy extends AppCompatActivity {
  Button pindahgambar, buttonbaru;
+ ImageView imageView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -17,12 +19,14 @@ public class challengekeduaudemy extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challengekeduaudemy);
         pindahgambar = findViewById(R.id.pindahgambar);
+        imageView2 = findViewById(R.id.imageView2);
 
         pindahgambar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent pindahdirec = new Intent(challengekeduaudemy.this,pindahgambar.class); // kenapa biki variabel gotosign?
-                startActivity(pindahdirec); // saat klik dia akan keSignAct activity
+//                Intent pindahdirec = new Intent(challengekeduaudemy.this,pindahgambar.class); // kenapa biki variabel gotosign?
+//                startActivity(pindahdirec); // saat klik dia akan keSignAct activity
+                imageView2.setImageResource(R.drawable.cat1);
             }
         });
     }
