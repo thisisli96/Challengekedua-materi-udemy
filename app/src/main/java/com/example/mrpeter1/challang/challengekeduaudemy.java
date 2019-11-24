87bc439e97bab414bbacac20ac6f1201a3fdf531
muh ikhsan <thisisli96@yahoo.com>
2019-11-24 08:29:00 +0700

    coba 2 challenge udemy

---
+++ app/src/main/java/com/example/mrpeter1/challang/challengekeduaudemy.java
@@ -0,0 +1,15 @@
+
+package com.example.mrpeter1.challang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class challengekeduaudemy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challengekeduaudemy);
+    }
+}
